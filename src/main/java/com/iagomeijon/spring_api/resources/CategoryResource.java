8771f407ebfafedb9028e96718b750cd7192a395
iagomeijon <iagomeijonm@gmail.com>
2020-06-14 11:12:07 -0300

    15 - added category domain

--- src/main/java/com/iagomeijon/spring_api/resources/CategoryResource.java
+++ src/main/java/com/iagomeijon/spring_api/resources/CategoryResource.java
@@ -1,15 +1,23 @@
 package com.iagomeijon.spring_api.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.iagomeijon.spring_api.domain.Category;
 
 @RestController
 @RequestMapping("/categories")
 public class CategoryResource {
 
 	@GetMapping
-	public String restTest() {
-		return "thats ok!";
+	public List<Category>restTest() {
+		Category c1 = new Category(1,"Computing");
+		Category c2 = new Category(2,"Office");
+		return new ArrayList<Category>(Arrays.asList(c1,c2));
 	}
 }
