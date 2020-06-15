6262cb7382e1acac83afcaa5b2aad7c876032790
iagomeijon <iagomeijonm@gmail.com>
2020-06-15 09:37:09 -0300

    21 - added state and city

--- src/main/java/com/iagomeijon/spring_api/config/Instatiation.java
+++ src/main/java/com/iagomeijon/spring_api/config/Instatiation.java
@@ -1,6 +1,5 @@
 package com.iagomeijon.spring_api.config;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +7,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import com.iagomeijon.spring_api.domain.Category;
+import com.iagomeijon.spring_api.domain.City;
 import com.iagomeijon.spring_api.domain.Product;
+import com.iagomeijon.spring_api.domain.State;
 import com.iagomeijon.spring_api.repositories.CategoryRepository;
+import com.iagomeijon.spring_api.repositories.CityRepository;
 import com.iagomeijon.spring_api.repositories.ProductRepository;
+import com.iagomeijon.spring_api.repositories.StateRepository;
 
 @Configuration
 public class Instatiation implements CommandLineRunner{
@@ -20,9 +23,17 @@ public class Instatiation implements CommandLineRunner{
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private StateRepository stateRepository;
+	
+	@Autowired
+	private CityRepository cityRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 		
+		//MARK: Product and category
 		Product p1 = new Product(null, "Computer", 3000.00);
 		Product p2 = new Product(null, "Priter", 250.99);
 		Product p3 = new Product(null, "Mouse", 80.00);
@@ -40,6 +51,21 @@ public class Instatiation implements CommandLineRunner{
 		
 		categoryRepository.saveAll(Arrays.asList(c1, c2 ,c3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		//MARK: State and city
+		State st1 = new State(null, "Bahia");
+		State st2 = new State(null, "São paulo");
+		
+		City city1 = new City(null, "Salvador", st1);
+		City city2 = new City(null, "São paulo", st2);
+		City city3 = new City(null, "Santos", st2);
+		
+		st1.getCities().add(city1);
+		st2.getCities().addAll(Arrays.asList(city2, city3));
+		
+		
+		stateRepository.saveAll(Arrays.asList(st1, st2));
+		cityRepository.saveAll(Arrays.asList(city1, city2, city3));
 	}
 
 }
